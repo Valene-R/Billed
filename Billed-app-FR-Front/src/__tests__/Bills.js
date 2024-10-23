36f98d26db02780b776506371a9f9dc57ad4c6dc
Valène Renaud <valene.renaud@gmail.com>
2024-10-23 15:54:44 +0200

    test(Bills): verify navigation to NewBill page on button click

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -3,12 +3,16 @@
  */
 
 import {screen, waitFor} from "@testing-library/dom"
+import userEvent from "@testing-library/user-event"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store" 
+import { ROUTES_PATH } from "../constants/routes.js"
+import {localStorageMock} from "../__mocks__/localStorage.js"
+import router from "../app/Router.js"
+import Bills from "../containers/Bills"
 
-import router from "../app/Router.js";
+jest.mock("../app/store", () => mockStore)
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -38,4 +42,36 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted)
     })
   })
-})
+
+  describe("When I click on NewBill button", () => {
+    // Test if clicking the 'New Bill' button navigates to the NewBill page
+    test("Then it should navigate to the NewBill page", async () => {
+      const onNavigate = jest.fn()  // Mock the navigation function
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+
+      router(); // Initialize the router
+      window.onNavigate(ROUTES_PATH.Bills) // Simulate navigating to the Bills page
+      
+      // Create an instance of Bills
+      const billsInstance = new Bills({ document, onNavigate, store: null, localStorage: window.localStorage })
+
+      // Wait for the 'New Bill' button to appear in the DOM
+      await waitFor(() => screen.getByTestId('btn-new-bill'))
+      const buttonNewBill = screen.getByTestId('btn-new-bill')
+      
+      // Verify the 'New Bill' button is present
+      expect(buttonNewBill).toBeTruthy() 
+      
+      // Simulate clicking the 'New Bill' button
+      const handleClickNewBill = jest.fn(billsInstance.handleClickNewBill)  
+      buttonNewBill.addEventListener('click', handleClickNewBill) 
+      userEvent.click(buttonNewBill)  
+
+      // Verify the click event and navigation
+      expect(handleClickNewBill).toHaveBeenCalled();  
+      expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH['NewBill'])  
+    });
+  });
+});
