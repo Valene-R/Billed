8269ab78d7913a0daa1617f665d99f15fd7de05c
Valène Renaud <valene.renaud@gmail.com>
2024-10-29 18:39:17 +0100

    test(NewBill): add unit tests for form and file input field display

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -9,10 +9,20 @@ import NewBill from "../containers/NewBill.js"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+    // Test if the NewBill form is displayed
+    test("Then the NewBill form should be displayed", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
-      //to-do write assertion
+      const form = screen.getByTestId("form-new-bill")
+      expect(form).toBeTruthy()
+    })
+
+    // Test if the file input field exists in the form
+    test("Then the file input should be in the form", () => {
+      const html = NewBillUI()
+      document.body.innerHTML = html
+      const fileInput = screen.getByTestId("file")
+      expect(fileInput).toBeTruthy()
     })
   })
 })
